3fbf2bec5f64b037635c15044a1e5e24fd96c8e9
EugeneKovtun <eugene.kraros@gmail.com>
2018-01-13 05:54:15 +0200

    added prototype of Personal Area

--- src/main/java/com/emelib/servlets/LoginServlet.java
+++ src/main/java/com/emelib/servlets/LoginServlet.java
@@ -4,13 +4,27 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.io.File;
 import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import java.io.PrintWriter;
 
 public class LoginServlet extends HttpServlet {
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processUser(req, resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.print("Login");
+        processUser(req, resp);
+    }
+
+    private void processUser(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("personalArea/index.html").forward(req, resp);
     }
 }
