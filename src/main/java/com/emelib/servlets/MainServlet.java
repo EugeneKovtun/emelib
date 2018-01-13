e930ec6dcd0ebc2b1e591b03bd97efd1058c5779
EugeneKovtun <eugene.kraros@gmail.com>
2018-01-13 05:52:21 +0200

    Added main servlet to return welcome page (it doesn't work)

---
+++ src/main/java/com/emelib/servlets/MainServlet.java
@@ -0,0 +1,20 @@
+package com.emelib.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter printWriter = response.getWriter();
+        printWriter.print("1111");
+
+
+    }
+
+}
