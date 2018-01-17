0f9cac2bdd2e3861acc1728511989a4aad9e0d0e
EugeneKovtun <eugene.kraros@gmail.com>
2018-01-17 03:23:36 +0200

    Added bookServlet

---
+++ src/main/java/com/emelib/servlets/BookServlet.java
@@ -0,0 +1,30 @@
+package com.emelib.servlets;
+
+import com.emelib.dao.BookDAO;
+import com.emelib.entities.Book;
+import com.emelib.utils.BookUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BookServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            int id = Integer.parseInt(req.getParameter("id"));
+            Book book = BookDAO.getById(id);
+            req.setAttribute("name", book.getName());
+            req.setAttribute("author", book.getAuthor());
+            req.setAttribute("price", book.getPrice());
+            req.setAttribute("text", BookUtils.getBookText(book, true));
+            req.getRequestDispatcher("book.jsp").forward(req, resp);
+        } catch (Exception e) {
+            req.getRequestDispatcher("404.html").forward(req, resp);
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/webapp/book.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: eugene
+  Date: 16/01/18
+  Time: 23:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Book</title>
+</head>
+<body>
+<div>${name}</div>
+<div>${author}</div>
+<div>${price}</div>
+<div>${text}</div>
+</body>
+</html>
