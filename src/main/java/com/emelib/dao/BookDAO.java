8b18d660d3971d75e06baa52962fae62877daa65
EugeneKovtun <eugene.kraros@gmail.com>
2018-01-18 02:58:17 +0200

    added method getAll()

--- src/main/java/com/emelib/dao/BookDAO.java
+++ src/main/java/com/emelib/dao/BookDAO.java
@@ -9,6 +9,8 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+import java.util.List;
+
 public class BookDAO {
     public static void addBook(Book book) {
         Configuration configuration = new Configuration()
@@ -38,5 +40,20 @@ public class BookDAO {
         Book book= session.get(Book.class, id);
         transaction.commit();
         return book;
+    }
+    public static List<Book> getAll() {
+        Configuration configuration = new Configuration()
+                .configure().addAnnotatedClass(Book.class)
+                .addAnnotatedClass(Account.class);
+        ServiceRegistry registry =
+                new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties())
+                        .build();
+        SessionFactory sessionFactory = configuration.buildSessionFactory(registry);
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Book> books = session.createCriteria(Book.class).list();
+        transaction.commit();
+        return books;
     }
 }
